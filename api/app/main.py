73c650536478c50b0a4bb5b1412ad56efcdf4fe1
Adeniyi <teebarg01@gmail.com>
2026-07-31 08:53:31 +0100

    [Feature]: fix cors

--- api/app/main.py
+++ api/app/main.py
@@ -38,7 +38,7 @@ app = FastAPI(
 app.middleware("http")(add_cache_headers)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_origins=settings.all_cors_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
